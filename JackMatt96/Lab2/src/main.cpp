f6f44e4e5c174d384b3e6d88adba634597844536
Mattia Giacomello <mattiagiacomello.mg@gmail.com>
2021-04-30 00:11:18 +0200

    Jack second lab

--- JackMatt96/Lab2/src/main.cpp
+++ JackMatt96/Lab2/src/main.cpp
@@ -1,150 +1,161 @@
 // Giacomello Mattia
 // I.D. 1210988
 
-
 #include <stdlib.h>
 #include <iostream>
-#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/imgproc.hpp>
+
+
+void findChessboardPoints(std::vector< cv::String >, std::vector<std::vector<cv::Vec2f>> , std::vector<cv::Mat> , std::vector< cv::String >);
+void calibrateCameraChessboard(std::vector<std::vector<cv::Vec2f>>,cv::Size, cv::Size , float ,cv::Mat , cv::Mat ,std::vector <double> ,std::vector <double> );
+std::vector<cv::Vec3f> chessboard3dPoints(cv::Size , float );
+cv::Mat undistortImage(cv::Mat , cv::Mat , cv::Mat );
 
 const int ESCKey = 27;
 const int windowHeight = 1024;
 const std::string orig_name = "Original photo. ESC to close";
 const std::string und_name = "Undistorted photo. ESC to close";
 
+cv::Size gridSize(7,5);
+float gridMeasure = 0.02;
+cv::String chessboardDirectory = "./data/",
+pattern = "*.jpg";
+
+
 int main(){
-	// User interface to ask user data
-	std::vector <std::string> chessNames;
-	std::string images_folder;
-	float gridSize;
-	int gridCols;
-	int gridRows;
 
-	std::cout << "Folder containing calibration images:\n->";
-	std::cin >> images_folder;
-	cv::glob(images_folder + "/*", chessNames);
-	std::cout << "Number of columns inner intersections:\n->";
-	std::cin >> gridCols;
-	std::cout << "Number of rows inner intersections:\n->";
-	std::cin >> gridRows;
-	std::cout << "Grid size in m:\n->";
-	std::cin >> gridSize;
+    std::vector< cv::String >imagesVectorPath;
+    cv::utils::fs::glob(chessboardDirectory,pattern,imagesVectorPath);
 
-	std::vector <cv::Mat> imagesChess;
-	std::vector <std::vector<cv::Vec2f>> image_points;
+	std::vector<std::vector<cv::Vec2f>> chessboardImagesPoints;
+    std::vector<cv::Mat> chessboardImages;
+    std::vector<cv::String >imagesName;
+	
+	findChessboardPoints(imagesVectorPath, chessboardImagesPoints, chessboardImages, imagesName);
+	
+    
+	cv::Mat cameraMatrix, distCoeffs;
+	std::vector <double> meanReprojectionErrors, meanSquaredReprojectionErrors;
 
-	std::cout << "\nSearching for images with chessboard of "+ std::to_string(gridCols)+"x"+ std::to_string(gridRows)+ " of size "+ std::to_string(gridSize)+"m in the folder "+ images_folder+
-		"\nChessboard found in: \n";
-	// Search for chessboard corners in the files in the path defined
-	for (size_t i = 0; i < chessNames.size(); i ++ )
-	{
-		cv::Mat img = cv::imread(chessNames[i]);
-		if (img.empty()) {
-			chessNames.erase(chessNames.begin() + i);
-			i--;
-			continue;
-		}
+	calibrateCameraChessboard(chessboardImagesPoints, chessboardImages[0].size(),gridSize, gridMeasure, cameraMatrix,  distCoeffs, meanReprojectionErrors, meanSquaredReprojectionErrors);
+
+	int minIdx, maxIdx, minSIdx, maxSIdx;
+	cv::minMaxIdx(meanReprojectionErrors,NULL,NULL,&minIdx,&maxIdx);
+	cv::minMaxIdx(meanSquaredReprojectionErrors,NULL,NULL,&minSIdx,&maxSIdx);
+
+    // Print to terminal of values obtained
+	std::cout << "\nIntrinsic Parameters: \n" << cameraMatrix
+		<< "\n\nLens coefficients: \n[k1 k2 p1 p2 k3] = " << distCoeffs
+		<< "\nBest image: " << imagesName[minIdx] <<" with mean reprojection error = "<< meanReprojectionErrors[minIdx] 
+		<< "\nWorst image: " << imagesName[maxIdx] << " with mean reprojection error = " << meanReprojectionErrors[maxIdx]
+		<< "\nBest image: " << imagesName[minSIdx] <<" with mean squared reprojection error = "<< meanSquaredReprojectionErrors[minSIdx] 
+		<< "\nWorst image: " << imagesName[maxSIdx] << " with mean squared reprojection error = " << meanSquaredReprojectionErrors[maxSIdx] << "\n";
+    
+
+    // Choice of distorted image to remap
+
+	std::string distortedImagePath;
+	std::cout << "Path of distorted image:\n->";
+	std::cin >> distortedImagePath;
+    
+	cv::Mat distortedImage = cv::imread(distortedImagePath);
+	while (distortedImage.empty()) {
+		std::cout << "Incorrect path, retry:\n->";
+		std::cin >> distortedImagePath;
+		distortedImage = cv::imread(distortedImagePath);
+	}
+	
+	cv::Mat undistortedImage = undistortImage(distortedImage, cameraMatrix, distCoeffs);
+
+	cv::resize(undistortedImage, undistortedImage, cv::Size(windowHeight * undistortedImage.cols / undistortedImage.rows, windowHeight));
+	cv::resize(distortedImage, distortedImage, cv::Size(windowHeight * distortedImage.cols / distortedImage.rows, windowHeight));	
+
+	// Visualization of distorted and undistorted images
+	cv::imshow(orig_name, distortedImage);
+	cv::imshow(und_name, undistortedImage);
+	while (cv::waitKey() != ESCKey); // Esc Key to close
+	return 0;
+}
+
+
+
+void findChessboardPoints(std::vector< cv::String >imagesVectorPath, std::vector<std::vector<cv::Vec2f>> chessboardImagesPoints, std::vector<cv::Mat> chessboardImages, std::vector< cv::String >imagesName){
+	for(auto imagePath:imagesVectorPath)
+    {
+        cv::Mat img = cv::imread(imagePath);
 		std::vector<cv::Vec2f> points;
-		if (cv::findChessboardCorners(img, cv::Size(gridCols, gridRows), points)) {
+		if (!img.empty() && cv::findChessboardCorners(img, gridSize, points)) {
 			// Refining of chessboard corner and push in the vector
 			cv::Mat gray;
 			cv::cvtColor(img, gray, cv::COLOR_RGB2GRAY);
 			cv::cornerSubPix(gray, points, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30, 0.01));
-			cv::drawChessboardCorners(img, cv::Size(gridCols, gridRows), points, true);
-			image_points.push_back(points);
-			imagesChess.push_back(img);
-			std::cout << chessNames[i]+"\n";
+			cv::drawChessboardCorners(img, gridSize, points, true);
+			chessboardImagesPoints.push_back(points);
+			chessboardImages.push_back(img);
+            imagesName.push_back(imagePath.substr(imagePath.find_last_of("/")));
 		}
-		else {
-			chessNames.erase(chessNames.begin()+i);
-			i--;
-		}
-	}
+    }
+}
+
+
+void calibrateCameraChessboard(std::vector<std::vector<cv::Vec2f>> chessboardImagesPoints,cv::Size imageSize, cv::Size gridSize, float gridMeasure,cv::Mat cameraMatrix, cv::Mat distCoeffs,std::vector <double> meanReprojectionErrors,std::vector <double> meanSquaredReprojectionErrors){
 	
-
-	// Creation of vector of virtual chessboard
-	std::vector<cv::Vec3f> chessTemp;
-	for (size_t i = 0; i < gridRows; i++)
-	{
-		for (size_t j = 0; j < gridCols; j++)
-		{
-			chessTemp.push_back(cv::Vec3f(j * gridSize, i * gridSize, 0));
-		}
-	}
-	std::vector<std::vector<cv::Vec3f>> chessboard(imagesChess.size(),chessTemp);
-
-
+	std::vector<cv::Vec3f> chessboard = chessboard3dPoints(gridSize,gridMeasure);
 	// Computation of camera intrinsic and extrinsic parameters and lens coefficients
-	cv::Mat cameraMatrix;
-	cv::Mat distCoeffs;
-	std::vector <cv::Mat> rvecs;
-	std::vector <cv::Mat> tvecs;
-	cv::calibrateCamera(chessboard, image_points, imagesChess[0].size(), cameraMatrix, distCoeffs, rvecs, tvecs);
+	
+	std::vector <cv::Mat> rvecs, tvecs;
+	cv::calibrateCamera(std::vector<std::vector<cv::Vec3f>> (chessboardImagesPoints.size(),chessboard), chessboardImagesPoints, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs);
 
+    // Computation of single root mean squared reprojection errors
+	meanReprojectionErrors = std::vector <double> (chessboardImagesPoints.size());
+    meanSquaredReprojectionErrors = std::vector <double> (chessboardImagesPoints.size());
 
-	// Computation of single root mean squared reprojection errors
-	std::vector <double> reprojectionErrors(imagesChess.size());
-	int minIdx = 0, maxIdx = 0;
-	double RMS = 0;
-
-	for (size_t i = 0; i < imagesChess.size(); i++)
+	for (size_t i = 0; i < chessboardImagesPoints.size(); i++)
 	{
-		std::vector <cv::Vec2f> chessReprojected;
-		cv::projectPoints(chessboard[0], rvecs[i], tvecs[i], cameraMatrix, distCoeffs, chessReprojected);
-		double error = 0;
-		for (size_t j = 0; j < chessReprojected.size(); j++)
+		std::vector <cv::Vec2f> chessboardReprojected;
+		cv::projectPoints(chessboard, rvecs[i], tvecs[i], cameraMatrix, distCoeffs, chessboardReprojected);
+		
+        double error = 0;
+		for (size_t j = 0; j < chessboardReprojected.size(); j++)
 		{
-			error += cv::pow(cv::norm(chessReprojected[j]- image_points[i][j]),2);
+			error += cv::norm(chessboardReprojected[j]- chessboardImagesPoints[i][j]);
+		}
+        meanReprojectionErrors[i] = error / chessboardImagesPoints[i].size();
+                
+        error = 0;
+		for (size_t j = 0; j < chessboardReprojected.size(); j++)
+		{
+			error += pow(cv::norm(chessboardReprojected[j]- chessboardImagesPoints[i][j]),2);
+		}
+        meanSquaredReprojectionErrors[i] = error / chessboardImagesPoints[i].size();        
+	}
+}
 
-		}
-		RMS += error;
-		reprojectionErrors[i] = cv::sqrt(error / chessboard[0].size());
-		if (reprojectionErrors[i]> reprojectionErrors[maxIdx])
+
+std::vector<cv::Vec3f> chessboard3dPoints(cv::Size gridSize, float gridMeasure){
+	// Creation of vector of virtual chessboard
+	std::vector<cv::Vec3f> chessboard;
+	for (size_t i = 0; i < gridSize.height; i++)
+	{
+		for (size_t j = 0; j < gridSize.width; j++)
 		{
-			maxIdx = i;
-		}
-		else if (reprojectionErrors[i] < reprojectionErrors[minIdx])
-		{
-			minIdx = i;
+			chessboard.push_back(cv::Vec3f(j * gridMeasure, i * gridMeasure, 0));
 		}
 	}
+	return chessboard;
+}
 
-	RMS = cv::sqrt(RMS / (chessboard[0].size() * chessboard.size()));
 
-	// Print to terminal of values obtained
-	std::cout << "\nReprojection Error: " << RMS
-		<< "\nIntrinsic Parameters: \n" << cameraMatrix
-		<< "\n\nLens coefficients: \n[k1 k2 p1 p2 k3] = " << distCoeffs
-		<< "\nBest image: " << chessNames[minIdx] <<" with RMS = "<< reprojectionErrors[minIdx] 
-		<< "\nWorst image: " << chessNames[maxIdx] << " with RMS = " << reprojectionErrors[maxIdx] << "\n";
-	
-	// Choice of distorted image to remap
-	std::string distortedName;
-	std::cout << "Folder containing calibration images:\n->";
-	std::cin >> distortedName;
-
-	cv::Mat imagesDistorted = cv::imread(distortedName);
-	while (imagesDistorted.empty()) {
-		std::cout << "Incorrect path, retry:\n->";
-		std::cin >> distortedName;
-		imagesDistorted = cv::imread(distortedName);
-	}
-	
+cv::Mat undistortImage(cv::Mat distortedImage, cv::Mat cameraMatrix, cv::Mat distCoeffs){
 	// Remapping of distorted images
 	cv::Mat map1, map2;
-	cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, cv::Mat(), cameraMatrix, imagesDistorted.size(), CV_32FC1, map1, map2);
+	cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, cv::Mat(), cameraMatrix, distortedImage.size(), CV_32FC1, map1, map2);
 	
-	cv::Mat imgUndist;
-	cv::remap(imagesDistorted, imgUndist, map1,map2, cv::INTER_LANCZOS4);
-	cv::resize(imgUndist, imgUndist, cv::Size(windowHeight * imgUndist.cols / imgUndist.rows, windowHeight));
-	cv::resize(imagesDistorted, imagesDistorted, cv::Size(windowHeight * imagesDistorted.cols / imagesDistorted.rows, windowHeight));
-	
-
-	// Visualization of distorted and undistorted images
-	cv::imshow(orig_name, imagesDistorted);
-	cv::imshow(und_name, imgUndist);
-	while (cv::waitKey() != ESCKey); // Esc Key to close
-	return 0;
+	cv::Mat undistortedImage;
+	cv::remap(distortedImage, undistortedImage, map1,map2, cv::INTER_LANCZOS4);
+	return undistortedImage;
 }
